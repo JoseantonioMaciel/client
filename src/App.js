6b4e042816b877a6374b3d7ca8389358bee5d176
JoseantonioMaciel <39782899+JoseantonioMaciel@users.noreply.github.com>
2022-02-07 14:43:28 -0800

    init update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,105 @@
 
 import './App.css';
-import Hero from "./pages/Hero/Hero";
-import Header from "./pages/Header/header";
-import MediaCard from "./pages/MediaCard/MediaCard";
-import Services from "./pages/services/Services";
-import PROCESS from "./pages/OurProcess/ProcessCards";
-import LETSBEGIN from "./pages/LetsBegin/LetsBegin";
-import Footer from "./pages/Footer/Footer";
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Footer from "./pages/Footer/Footer"
 
-
-
-function App() {
+export default function App() {
   return (
-
-    <div >  
-    
-    <Header/>
-    <Hero/>
-    <MediaCard/>
-    <Services/>
-    <PROCESS/>
-    <LETSBEGIN/>
-    <Footer/>
+    <div>
+      <h1> Nave</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link to="/about">About UDS</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact US</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+function Services() {
+  return (
+    <div>
+      <h2>Services</h2>
+    </div>
+  );
+}
+
+function Gallery() {
+  return (
+    <div>
+      <h2>Gallery</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
